51221a9c287d902a8a03b3ad80904d7fc239c57a
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-11 16:11:32 +0100

    Mensajes
    It storage it in a "File" object

---
+++ src/exercises/Mensajes.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package exercises;
+
+import java.io.*;
+
+public class Mensajes {
+
+	
+	public static void main(String[] args) {
+		File poem = new File ("los_zapaticos_de_rosa.txt");
+
+		FileReader reader = null;
+		BufferedReader buffer = null;
+		
+		try {
+			reader = new FileReader(poem);
+			buffer = new BufferedReader(reader);
+			
+			String linea;
+			while( (linea=buffer.readLine()) != null) {
+			System.out.println(linea);
+				}
+		}
+		catch(Exception err){
+			err.printStackTrace();
+			}
+	}
+
+}
--- src/testing/Reading101.java
+++ src/testing/Reading101.java
@@ -3,13 +3,13 @@
  */
 package testing;
 
-import java.io.*;
+//import java.io.*;
 
 public class Reading101 {
 
 	
 	public static void main(String[] args) {
-		File f_verso = new File ("archivo.cfg");
+		//File f_verso = new File ("archivo.cfg");
 
 	}
 
