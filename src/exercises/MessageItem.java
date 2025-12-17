9c5030a1515526614567dce108ec53c8297f2edb
beatrizcalzadillaso <beatriz.calzadillaso@elorrieta-errekamari.com>
2025-12-17 21:22:52 +0100

    adde new class item

---
+++ src/exercises/MessageItem.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package exercises;
+
+/**
+ * 
+ */
+public class MessageItem {
+	private String fecha;
+	private String hora;
+	private String de;
+	private String para;
+	private String asunto;
+	private String contenido;
+	/**
+	 * @return the fecha
+	 */
+	private String getFecha() {
+		return fecha;
+	}
+	/**
+	 * @param fecha the fecha to set
+	 */
+	private void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+	/**
+	 * @return the hora
+	 */
+	private String getHora() {
+		return hora;
+	}
+	/**
+	 * @param hora the hora to set
+	 */
+	private void setHora(String hora) {
+		this.hora = hora;
+	}
+	/**
+	 * @return the de
+	 */
+	private String getDe() {
+		return de;
+	}
+	/**
+	 * @param de the de to set
+	 */
+	private void setDe(String de) {
+		this.de = de;
+	}
+	/**
+	 * @return the para
+	 */
+	private String getPara() {
+		return para;
+	}
+	/**
+	 * @param para the para to set
+	 */
+	private void setPara(String para) {
+		this.para = para;
+	}
+	/**
+	 * @return the asunto
+	 */
+	private String getAsunto() {
+		return asunto;
+	}
+	/**
+	 * @param asunto the asunto to set
+	 */
+	private void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+	/**
+	 * @return the contenido
+	 */
+	private String getContenido() {
+		return contenido;
+	}
+	/**
+	 * @param contenido the contenido to set
+	 */
+	private void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	
+	
+
+}
